9431588890a3d69e47a08c7cc091843e55355ee1
Jesse Snyder <jsnyder@wesleyan.edu>
2020-05-27 04:35:46 -0700

    flake8 config

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -8,6 +8,7 @@ def test_end_to_end():
     result = Euchre15.run(have_real_player=False, games=num_games, ui=ui)
     scores = [result[0].gamescore, result[1].gamescore]
     assert scores[0] + scores[1] == num_games
+    assert len(scores) == num_games
 
 
 def test_end_to_end_with_fake_real_player():
